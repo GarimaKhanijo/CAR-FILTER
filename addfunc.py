8128bab7e01c638cb73e7e6e74d7c041dd75f842
Garima <123671271+GarimaKhanijo@users.noreply.github.com>
2023-03-23 13:19:37 +0530

    Add files via upload

---
+++ addfunc.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def add(d,output_d):
+  global budget,fuel_type,body_type,transmission
+  a=input("Model Name(Enter a string): ")
+  b=input("Manufacturer(Enter a string): ")
+  c=int(input("Base Price(Enter a number): "))
+  try1()
+  filter_1=filter01(d,budget)
+  filter_2=filter_1[body_type]
+  filter_3=filter_2[transmission]
+  filter_4=filter_3[fuel_type]
+  filter_4.append(a)
+  output_d[a]=[b,str(c)]
+  print("\nData has been added.\n")
+  
+  
+
